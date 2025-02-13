7b01c8798bb778aa60f09e308421d6226eaa32db
Lucas García <lucasgarciabaro@protonmail.com>
2025-02-13 22:14:48 +0100

    feat: work breadcrumb

---
+++ patterns/artwork-breadcrumb.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: Artwork Breadcrumb
+ * Slug: oriol-mora/artwork-breadcrumb
+ * Categories: general
+ *
+ * @package Oriol Mora Theme
+ * @since 1.0.0
+ */
+?>
+<!-- wp:paragraph {"style":{"spacing":{"margin":"top":"2rem"}}}-->
+<p style="margin-top: 2rem">
+  <a
+	style="
+	  text-decoration: none;
+	  font-weight: bold;
+	  color: var(--wp--preset--color--accent);
+	  cursor: pointer;
+	"
+	onclick="history.back()"
+  >
+	<?php echo __('← Tornar', 'oriol-mora'); ?>
+  </a>
+</p>
+<!-- /wp:paragraph -->
