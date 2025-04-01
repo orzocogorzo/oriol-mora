f850c261b18c83aa50520d2859c37ef5b7e03eda
Lucas García <lucasgarciabaro@protonmail.com>
2025-04-02 00:26:35 +0200

    feat: update translations

---
+++ languages/es_ES.l10n.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'project-id-version' => 'Oriol Mora',
+    'report-msgid-bugs-to' => '',
+    'pot-creation-date' => '2025-04-01 18:38+0000',
+    'po-revision-date' => '2025-04-01 18:45+0000',
+    'last-translator' => 'Lucas García',
+    'language-team' => 'Spanish (Spain)',
+    'language' => 'es_ES',
+    'plural-forms' => 'nplurals=2; plural=n != 1;',
+    'mime-version' => '1.0',
+    'content-type' => 'text/plain; charset=UTF-8',
+    'content-transfer-encoding' => '8bit',
+    'x-generator' => 'Loco https://localise.biz/',
+    'x-loco-version' => '2.7.1; wp-6.7.2',
+    'x-domain' => 'oriol-mora',
+    'messages' => ['← Tornar' => '← Volver'],
+];
